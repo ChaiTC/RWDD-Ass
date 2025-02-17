4640055870d4388a7cdaf1525f006bfb9f0e594f
ChaiTC <tianchengctc5@gmail.com>
2025-02-17 13:52:42 +0800

    Delete financial_tips.js

--- financial_tips.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const menuBtn = document.querySelector(".menu-btn"); 
-    const sidebar = document.querySelector(".sidebar");
-});
-
-menuBtn.addEventListener("click", function () {
-    sidebar.classList.toggle("active");
-});
