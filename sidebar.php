523d7bf145c7b2217cf58fd1c1e6568b548db1c7
ChaiTC <tianchengctc5@gmail.com>
2025-02-20 00:31:01 +0800

    1

--- sidebar.php
+++ sidebar.php
@@ -11,6 +11,6 @@
         <li><a href="quiz_Page.php"><img src="quiz-icon.png" alt="Quiz"> <span>QUIZ</span></a></li>
         <li><a href="financial_tips.php"><img src="tips-icon.png" alt="Tips"> <span>TIPS</span></a></li>
         <li><a href="aboutus.php"><img src="about_us.png" alt="About Us"> <span>ABOUT US</span></a></li>
-        <li><a href="logout.php"><img src="logout.png" alt="Log out"> <span>Log Out</span></a></li>
+        <li><a href="logout.php"><img src="logout.png" alt="Log out"> <span>LOG OUT</span></a></li>
     </ul>
 </div>
