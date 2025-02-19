1ff4de089cb1a88f496731e575eea0d4ce21b43e
Dshooi <desmondhooi1123@gmail.com>
2025-02-19 12:43:26 +0800

    update

--- register.php
+++ register.php
@@ -56,7 +56,7 @@ if (\$_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login & Register</title>
-    <link rel="stylesheet" href="login&register_style.css">
+    <link rel="stylesheet" href="register_style.css">
 </head>
 <body>
     <div class="logo">
